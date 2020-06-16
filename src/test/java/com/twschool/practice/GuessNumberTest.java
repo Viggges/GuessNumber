da434f13752df06605d38844bbf30490bddaba4f
lw <623656827@qq.com>
2020-06-16 20:14:19 +0800

    全程有值对 有全对的测试

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -27,4 +27,52 @@ public class GuessNumberTest {
 
         System.out.println(result + "   "+"all wrong");
     }
+    @Test
+    public void should_return_1A2B_input_2436() {
+        //given
+        int[] input = {2,4,3,6};
+        int[] answer = {1,2,3,4};
+        //when
+        String result = Game.playGame(input,answer);
+        //then
+        Assert.assertEquals("1A2B", result);
+
+        System.out.println(result + "   "+"all wrong");
+    }
+    @Test
+    public void should_return_0A1B_input_2789() {
+        //given
+        int[] input = {2,7,8,9};
+        int[] answer = {1,2,3,4};
+        //when
+        String result = Game.playGame(input,answer);
+        //then
+        Assert.assertEquals("0A1B", result);
+
+        System.out.println(result + "   "+"all wrong");
+    }
+    @Test
+    public void should_return_1A0B_input_1789() {
+        //given
+        int[] input = {1,7,8,9};
+        int[] answer = {1,2,3,4};
+        //when
+        String result = Game.playGame(input,answer);
+        //then
+        Assert.assertEquals("1A0B", result);
+
+        System.out.println(result + "   "+"all wrong");
+    }
+    @Test
+    public void should_return_0A4B_input_4321() {
+        //given
+        int[] input = {4,3,2,1};
+        int[] answer = {1,2,3,4};
+        //when
+        String result = Game.playGame(input,answer);
+        //then
+        Assert.assertEquals("0A4B", result);
+
+        System.out.println(result + "   "+"all wrong");
+    }
 }
