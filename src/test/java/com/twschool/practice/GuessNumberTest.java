c3d8119ea0078d5fa7ac0f264163b4909bc26d0e
lw <623656827@qq.com>
2020-06-16 20:08:15 +0800

    完成全错的测试

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -13,6 +13,18 @@ public class GuessNumberTest {
         String result = Game.playGame(input,answer);
         //then
         Assert.assertEquals("4A0B", result);
-        System.out.println("win, all correct");
+        System.out.println(result+ "   "+"win, all correct");
+    }
+    @Test
+    public void should_return_0A0B_input_5678() {
+        //given
+        int[] input = {5,6,7,8};
+        int[] answer = {1,2,3,4};
+        //when
+        String result = Game.playGame(input,answer);
+        //then
+        Assert.assertEquals("0A0B", result);
+
+        System.out.println(result + "   "+"all wrong");
     }
 }
