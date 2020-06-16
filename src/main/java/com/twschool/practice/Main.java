0eb730bd38c24672df1b4caf6a657e3283dab921
lw <623656827@qq.com>
2020-06-16 20:33:06 +0800

    添加游戏次数

--- src/main/java/com/twschool/practice/Main.java
+++ src/main/java/com/twschool/practice/Main.java
@@ -6,15 +6,17 @@ import java.util.Scanner;
  * @author Administrator
  */
 public class Main {
+    private static final int RUN_TIMES = 6;
 
     public static void main(String[] args) throws Exception {
-        int[] input = new int[4];
-        int[] answer = {1, 2, 3, 4};
-        Scanner inputScanner = new Scanner(System.in);
-        for (int i = 0; i < 4; i++) {
-            input[i] = inputScanner.nextInt();
+        for (int time = 0; time < RUN_TIMES; time++) {
+            int[] input = new int[4];
+            int[] answer = {1, 2, 3, 4};
+            Scanner inputScanner = new Scanner(System.in);
+            for (int i = 0; i < 4; i++) {
+                input[i] = inputScanner.nextInt();
+            }
+            System.out.println(Game.playGame(input, answer));
         }
-        System.out.println(Game.playGame(input, answer));
-
     }
 }
